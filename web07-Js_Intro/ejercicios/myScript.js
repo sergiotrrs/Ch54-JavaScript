9e89be41cc6445f12c9b678f07e5afd02ec5c239
Yessica Pérez <yessicaperez0598@gmail.com>
2025-05-12 16:05:11 -0600

    ejercicios_clase

---
+++ web07-Js_Intro/ejercicios/myScript.js
@@ -0,0 +1,23 @@
+function saludar() {
+    alert("Ya casi nos vamos al segundo descanso")
+}
+
+function bodyColor( color ){
+    document.body.style.backgroundColor = color;
+}
+
+function changeToGreenColor(){
+    const element = document.getElementById("green-paragraph");
+    console.log(element);
+    element.style.color= "green";
+}
+
+function changeColor (element, color) {
+    element.style.color = color;
+}
+
+function setUp(){
+    document.getElementById("title").style.color = "#e912df";
+    document.getElementById("title").style.fontSize = "50px";
+};
+setUp();
