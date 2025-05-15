640cc29f660f5c535eb8e0854317ed695b2f19cf
Sergio Torres <sergio.torres@idr.edu.mx>
2025-05-15 17:12:12 -0600

    feat: ejercicio ventilador

--- js-03-control_flow/src/js/main.js
+++ js-03-control_flow/src/js/main.js
@@ -42,6 +42,78 @@
 
 */
 
+/* 
+ Crear un arrow function que reciba el valor de edad.
+ Si la edad es mayor o igual a 18, la función debe
+ retornar el mensaje "Eres mayor de edad".
+
+ En caso contrario, retornar el mensaje "Eres menor de edad".
+
+ - Realizar la versión de if-else sin usar el bloque de código {}
+*/
+const verificarEdad = edad => {
+    if (edad >= 18) return "Eres mayor de edad"
+    else return "Eres menor de edad";
+}
+const resultado = verificarEdad(19);
+console.log(resultado);
+
+/*
+ Crear un arrow function que reciba el valor de edad.
+ Si la edad es mayor o igual a 18 y menor o igual a 30
+                 ( evalur con 18 Y(&&) evaluar con 30        )
+ la función debe retornar el mensaje "Tu luchador es Místico".
+
+ Si es mayor o igual a 31 el mensaje "Tu luchador es el perro Aguayo".
+
+ Si es menor a 18 el mensaje "Tu luchador es Penta"
+
+*/
+const verificarEdad2 = (edad) =>{
+    if (edad >= 18 && edad <= 30) return "Tu luchador es místico.";
+    else if (edad >= 31) return "Tu luchador es el perro guayo.";
+    else return "Tu luchador es penta.";
+};
+console.log(verificarEdad2(25));
+
+// Bloque de código
+{
+    let myVarLet = 30;
+    const myVarConst = 40;
+    var myVarVar = 50;
+    console.log(myVarLet, myVarConst, myVarVar); // 30,40,50
+    
+    {
+        let myVarLet = 100;
+        const myVarConst = 110;
+        var myVarVar = 120;
+        console.log(myVarLet, myVarConst, myVarVar); // 100,110,120
+    }
+    console.log(myVarLet, myVarConst, myVarVar);// 30,40,120
+}
+
+
+// Ejercicios mentales
+let active = false
+if( active === true ); {
+  console.log("Está activo")
+}
+console.log("Fin del programa");
+
+// --------------Ejercicio 2-----------------
+active = false;
+if( active )
+  console.log("Está activo");
+  console.log("Tiene autorización");
+
+console.log("Fin del programa");
+
+// --------------Ejercicio 3-----------------
+
+active = false;
+if( active ); console.log("Está activo");
+//else console.log("No está activo"); // Unexpected token 'else'
+// console.log("Fin del programa");
 
 
 
@@ -72,8 +144,35 @@
   }
 
 */
+const evaluarNombre = ( nombre ) => {
+    let resultado;
+    switch ( nombre){
+        case "Lua":
+            resultado = "Es de Abril";
+            break;
+        case "Borrego": 
+        case "Besito":
+        case "Fijól":
+        case "Paco":
+            resultado = "Es de Yessica";
+            break;
+        case "Milo":
+            resultado = "Es de Carlos";
+            break
+        case "Cuchara":
+            resultado = "Es de Ricardo";
+            break;
+        default:
+            resultado ="No se sabe de quién es";        
+    }
+    return resultado;
+}
+console.log( evaluarNombre("Lua") ); // Es de Abril
+console.log( evaluarNombre("Milo") ); // Es de Carlos
+console.log( evaluarNombre("Borrego") ); // Es de Jessica
 
 
+const nombreMascota = "Lua";
 
 
 /**
@@ -86,6 +185,8 @@
  * Cualquier otro valor: Velocidad desconocida
  * @param {number} velocidad
  * @returns {string} mensaje
+ * 
+ * Realizar dos versiones, una versión con switch y otra con if-else
  */
 
 
