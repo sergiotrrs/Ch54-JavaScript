569b4f9b2dfa4fd099ba56c59c68af693434bc5e
Jenipher Mares <jeniphermares@gmail.com>
2025-05-19 15:46:24 -0600

    New exampĺes to topics added

--- js-04-arrays_and_loops/src/js/main.js
+++ js-04-arrays_and_loops/src/js/main.js
@@ -221,29 +221,61 @@ const elNumeroExisteEnArreglo = (arreglo, numero) => {
 }
 
 //
-const imprimirMelateChocolate = (numeros) => {
+const imprimirMelateChocolate = ( numeros ) => {
     const referencia = document.getElementById("melate-chocolate");
-    referencia.innerHTML = `${numeros.join(' - ')}`;
-};
-
-const generarNumeroAleatorio = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const elNumeroExisteEnArreglo = (arreglo, numero) => {
-    return arreglo.includes(numero);
-};
-
+    referencia.innerHTML = ` ${numeros.join(' - ')} `;
+}
 const generarNumerosDeLaSuerte = (size = 6, minNum = 1, maxNum = 54) => {
     const numeros = [];
-    let iteracion = 0;
-    while (numeros.length < size) {
+    while( numeros.length < size ){
         const numAleatorio = generarNumeroAleatorio(minNum, maxNum);
-        if (!elNumeroExisteEnArreglo(numeros, numAleatorio)) {
+        if( elNumeroExisteEnArreglo(numeros, numAleatorio) === false ){
             numeros.push(numAleatorio);
         }
-        iteracion++;
-        console.log("Iteración:", iteracion);
+            iteracion++;
+    console.log(iteracion);
     }
-    imprimirMelateChocolate(numeros);
-};
+    imprimirMelateChocolate( numeros);
+
+}
+
+// ---------- Uso del método sort() -----------------
+
+const numerosIniciales = [ 5, 45, 82, 12, 23, 45, 100, 1 ];// regresaria esto
+                       //[ 5, 33, 8, 100, 4, 2, 7, 6 ] iteración 0
+                       //[ 5, 8, 33, 100, 4, 2, 7, 6 ] iteración 1
+                       //[ 5, 8, 33, 100, 4, 2, 7, 6 ] iteración 2
+                       //[ 5, 8, 33, 4, 100, 2, 7, 6 ] iteración 3
+                       //[ 5, 8, 33, 4, 2, 100, 7, 6 ] iteración 4
+                       //[ 5, 8, 33, 4, 2, 7, 100, 6 ] iteración 5
+                       //[ 5, 8, 33, 4, 2, 7, 6, 100 ] iteración 6
+
+// --------- organización de números de forma ascendente --------------
+/// --------- organización de números de forma ascendente -------------
+const comparaNumeros = ( a, b ) => {
+    if ( a < b ) return -1;
+    if ( a > b ) return 1;
+    return 0;
+}
+
+const comparaNumeros2 = ( a, b ) => a - b;
+
+// --------- organización de números de forma descendente --------------
+const compararNumerosDesendentes = (a, b) => {
+   if ( a > b ) return -1;
+    if ( a < b ) return 1;
+}
+
+const compararNumerosDesendentes2 = ( a, b ) => b - a; 
+
+const ordenarNumeros = ( numerosDesordenados, fncCallBack )=>{ 
+    const numerosOrdenados = numerosDesordenados;
+    numerosOrdenados.sort( fncCallBack ); // llama a la función sin cambiar 
+    return numerosOrdenados;
+}
+console.log( numerosIniciales );
+console.log( ordenarNumeros(numerosIniciales, compararNumerosDesendentes) ); // la 2 es la llamada
+//-------------------------(        1       ,    esta es la 2           ) )
+console.log([33, 23, 45, 80], comparaNumeros);
+console.log([33, 23, 45, 80], compararNumerosDesendentes);
+console.log([33, 23, 45, 80], compararNumerosDesendentes2);
