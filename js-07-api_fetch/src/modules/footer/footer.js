5acb09afca58b30b8ecf71cc5a4109667492aa4a
Sergio Torres <sergio.torres@idr.edu.mx>
2025-05-21 10:32:25 -0600

    feat: boilerplate js07

---
+++ js-07-api_fetch/src/modules/footer/footer.js
@@ -0,0 +1,11 @@
+
+const insertMainFooter = ( footer )=>{
+
+    footer.innerHTML = `
+        <div class="container text-center">
+            <p>&copy; 2025 JavaScript Course</p>
+        </div>
+    `;
+
+}
+
