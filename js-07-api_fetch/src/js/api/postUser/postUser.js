aaed819917a2d5b6aa573b5edf1f58568046ea1f
Sergio Torres <sergio.torres@idr.edu.mx>
2025-05-26 14:39:55 -0600

    feat: método post para enviar datos a la api del servidor

---
+++ js-07-api_fetch/src/js/api/postUser/postUser.js
@@ -0,0 +1,25 @@
+
+const postUser = async ( user, url ) => {
+    // Para el método POST, PUT y DELETE se debe configurar
+    // el objeto de configuración del meétodo fetch.
+    const options = {
+        method: "POST", // Método HTTP: POST, PUT, DELETE, GET
+        headers: {
+            "Content-Type": "application/json" // Tipo de contenido
+        },
+        body: JSON.stringify(user) // Cuerpo de la petición
+    }
+
+    const response = await fetch( url, options);
+    console.log( "Respuesta del servidor:", response );
+    if ( !response.ok || response.status !== 201 ) {
+        // Si la respuesta no es correcta, lanzar un error
+        throw new Error(`Error al enviar el usuario: ${response.statusText}`);
+    }
+
+    // Solo es necesario si el servidor devuelve un cuerpo en la respuesta
+    // en formato JSON.
+    const data = await response.json(); 
+    return data;
+}
+export { postUser };
--- js-07-api_fetch/src/pages/register/register.js
+++ js-07-api_fetch/src/pages/register/register.js
@@ -1,3 +1,4 @@
+import { postUser } from "../../js/api/postUser/postUser.js";
 import { isUserValid } from "../../js/validations/isUserValid.js";
 import { insertMainHeader as menu } from "../../modules/header/header.js";
 import { leerInputsFormulario } from "./leerInputsFormulario.js";
@@ -10,7 +11,7 @@ menu( document.getElementById("header") );
 const registerForm = document.getElementById("contactForm");
 
 // registerForm.addEventListener( "evento" , ()=>{} );
-registerForm.addEventListener( "submit" , ( e )=>{
+registerForm.addEventListener( "submit" , async ( e )=>{
     e.preventDefault(); // evita que se envíe el formulario
     console.log("Manejo del formulario");
     // console.log( e );
@@ -21,16 +22,20 @@ registerForm.addEventListener( "submit" , ( e )=>{
      const validatedUser = isUserValid( newUser );
     if ( validatedUser.isValid ){
         // Si los datos son válidos enviar el formulario
-        
+        try {
+            const response = await postUser(newUser, "https://reqres.in/api/users"); 
+            alert("Formulario enviado correctamente " + response.createdAt);           
+        } catch (error) {
+            alert("Error al enviar el formulario: " + error.message);
+        }
+        // = Enviar un mensaje al usuario, limpiar el formulario, redirigir la página
         // - Usando la api fetch al servidor, con el método POST enviar el formulario
     } else {
+        // Si los datos no son válidos enviar un mensaje al usuario
         alert( validatedUser.errors.join("\n") );
     }
 
-           // = Enviar un mensaje al usuario, limpiar el formulario, redirigir la página
+    // = Enviar al usuario el error del servidor
 
-           // = Enviar al usuario el error del servidor
-
-    // Si los datos no son válidos enviar un mensaje al usuario
 
 } );
