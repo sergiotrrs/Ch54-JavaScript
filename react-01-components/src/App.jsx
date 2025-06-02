fefe8e52aae7837081e873d731b5c6143109d5e2
Sergio Torres <sergio.torres@idr.edu.mx>
2025-06-02 10:43:59 -0600

    feat: uso de react router

--- react-01-components/src/App.jsx
+++ react-01-components/src/App.jsx
@@ -1,18 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Footer } from "./pages/footer/footer";
 import { Home } from "./pages/home/home";
 import { Navbar } from "./pages/navbar/navbar";
 import { Products } from "./pages/products/products";
+import { NotFound } from "./pages/not-found/notFound";
+
+/*
+  BrowserRouter sirve para habilitar el enrutamiento basado en 
+  la URL del navegador en tu aplicación React.
+
+  Utiliza la API de Historial de HTML5 para:
+   - Mantener la UI sincronizada con la URL: Cuando cambias de "página" 
+   en tu aplicación, BrowserRouter actualiza la URL en la barra de 
+   direcciones del navegador.
+   - Permitir la navegación con los botones de "atrás" y "adelante" 
+   del navegador: Escucha los cambios en el historial del navegador
+    y actualiza la UI correspondientemente.
+*/
 
 function App() {
   const myJsx = (
-    <>
-      <Navbar title="Día de Harry potter" />
-      <Home name="Myrtle la llorona" hogwartsHome="Ravenclaw" />      
-      <Home name="Sercherus Snape" hogwartsHome="Slytherin" />
-      <Products />
+    <BrowserRouter> {/* Habilita el enrutamiento */} 
+      {/*Navbar se mostrará para todas las rutas*/}
+      <Navbar title="Día de Harry potter" />      
+      <Routes> {/*Define y agrupa un conjunto de rutas*/}
+        <Route path="/" element={<Home name="Myrtle la llorona" hogwartsHome="Ravenclaw" />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="*" element={ <NotFound />} />
+      </Routes>
       <Footer cohorte={54} />
-    </>
+    </BrowserRouter>
   );
 
   return myJsx;
---
+++ react-01-components/src/pages/not-found/notFound.jsx
@@ -0,0 +1,8 @@
+
+const NotFound = () => {
+  return (
+    <div>notFound</div>
+  )
+};
+
+export { NotFound}
